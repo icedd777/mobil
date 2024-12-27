b43791bf5f477152388c3876e91d75b733d25c37
icedd777 <dumatubunrisat@gmail.com>
2024-12-27 17:22:37 +0700

    Add files via upload

---
+++ mobilBeraksi.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mobil;
+
+/**
+ *
+ * @author Risat Dumatubun
+ */
+public class mobilBeraksi {
+    public static void main(String[] args) {
+        mobil mobilrisat = new mobil();
+        
+        mobilrisat.warna = "biruu";
+        mobilrisat.tahunProduksi = 2011;
+        
+        System.out.println("warna : " + mobilrisat.warna);
+        System.out.println("tahun produksi : " + mobilrisat.tahunProduksi);
+    }
+    
+}
+    
+
